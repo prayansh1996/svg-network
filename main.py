b70f30ab1cb18ea41cdcd91d8a36ab91e354d45d
Prayansh Srivastava <prayansh@Prayanshs-MacBook-Pro.local>
2018-02-19 01:57:16 +0530

    names

--- main.py
+++ main.py
@@ -3,25 +3,36 @@ from subprocess import call
 import os
 import re
 import shutil
+import pickle
 
 makeSVGs = True
 moveSVGs = True
 extractd = True
 
+
+fontNames = set()
 if makeSVGs:
     os.chdir('./fonts')
     fonts = os.listdir('./')
     for font in fonts:
+        fontNames.add(font)
         escapedSpacesFont = font.replace(' ', '\ ')
-        os.system("fontforge -lang=ff -c 'Open($1); SelectWorthOutputting(); Select(65); Export(\"svg\"); endloop;' " + escapedSpacesFont)
+        os.system("fontforge -lang=ff -c 'Open($1); SelectWorthOutputting(); Select(105); Export(\"svg\"); endloop;' " + escapedSpacesFont)
     os.chdir('../')
 
 if moveSVGs:
     pathForSVGs = './svg'
     files = os.listdir('./fonts')
     for f in files:
-        if f.startswith('$'):
+        if not fontNames.__contains__(f):
             shutil.move(os.path.join('./fonts',f), pathForSVGs)
+
+class Fonts:
+    def __init__(self, name, d):
+        self.name = name
+        self.d = d
+
+fontList = []
 
 if extractd:
     files = os.listdir('./svg')
@@ -31,7 +42,8 @@ if extractd:
             d = []
             for ele in content.findall('.//{http://www.w3.org/2000/svg}path'):
                 d = ele.attrib['d']
-            print f, "-->", d
+            fontList.append(Fonts(f,d))
         except:
             print("Cannot parse: ", f)
         print ""
+
